135cc7c099071fb950509386bb614846f255c6a6
byakuyafox1 <119981967+byakuyafox1@users.noreply.github.com>
2023-08-31 12:28:56 -0300

    modificacion2

--- Api/src/Models/Users.js
+++ Api/src/Models/Users.js
@@ -1,1 +1,28 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const db_1 = require("../db");
+class Users extends sequelize_1.Model {
+}
+Users.init({
+    id: {
+        type: sequelize_1.DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: sequelize_1.DataTypes.UUIDV4,
+    },
+    name: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    password: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+}, {
+    sequelize: db_1.sequelize,
+    modelName: "Users",
+    timestamps: false,
+});
+exports.default = Users;
--- Api/src/db.js
+++ Api/src/db.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sequelize = void 0;
+const sequelize_1 = require("sequelize");
+const config_1 = require("../config"); // Asegúrate de que tienes tu archivo config.ts con los valores de entorno
+const sequelize = new sequelize_1.Sequelize(`postgres://${config_1.DB_USER}:${config_1.DB_PASSWORD}@${config_1.DB_HOST}/${config_1.DB_NAME}`, {
+    logging: false,
+    native: false,
+});
+exports.sequelize = sequelize;
