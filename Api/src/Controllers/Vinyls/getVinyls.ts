f369ed7ffa052afe4e55731fd0b82a875e18362a
Moonwalker231102 <ejimenezcadena@gmail.com>
2023-08-31 10:36:42 -0500

    Actualización del modelo users

--- Api/src/Controllers/Vinyls/getVinyls.ts
+++ Api/src/Controllers/Vinyls/getVinyls.ts
@@ -9,7 +9,7 @@ import {
 
 const getAllVinyls = async (req: Request, res: Response) => {
     try {
-        const { data } = await axios.get(`${URL}database/search?format=Vinyl&token=${TOKEN}`)
+        const { data } = await axios.get(`${URL}database/search?format=Vinyl&token=${TOKEN}&page=100&per_page=100`)
         fs.writeFileSync('response.json', JSON.stringify(data, null, 2));
         return res.status(200).json(data);
     } catch (error) {
