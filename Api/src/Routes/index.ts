7e33a4934fb5b4ae0022e4645dbf8fdf88e48e64
judaargu <judaargu@hotmail.com>
2023-09-01 15:38:31 -0500

    PostVinyls

--- Api/src/Routes/index.ts
+++ Api/src/Routes/index.ts
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import getAllVinyls from "../Controllers/Vinyls/getVinyls";
+import { postVinyl } from "../Controllers/Vinyls/postVinyl";
+import { Request, Response } from "express";
 
 const router = Router();
 
 router.get('/', getAllVinyls);
+
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const vinyl = await postVinyl(req.body);
+        return res.status(vinyl.status).json(vinyl.json);
+
+    } catch (error) {
+        return res.status(404).json(error);
+        
+    }
+})
 
 export default router;
