3162f914dd4ed727cc29730f84dbe550d72ce25a
GMZ-Santi <santugomeztrgmail.com>
2023-09-07 12:09:14 -0300

    Arreglos

--- Api/src/Routes/index.js
+++ Api/src/Routes/index.js
@@ -15,7 +15,6 @@ const postUser_1 = require("../Controllers/Users/postUser");
 const authMiddleware_1 = require("../Middlewares/authMiddleware");
 const postVinyl_1 = require("../Controllers/Vinyls/postVinyl");
 const router = (0, express_1.Router)();
-
 router.get("/", getVinyls_1.getAllVinyls);
 //! Ruta para registrar un nuevo usuario
 router.post("/createUser", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -25,6 +24,15 @@ router.post("/createUser", (req, res) => __awaiter(void 0, void 0, void 0, funct
     }
     catch (error) {
         return res.status(500).json({ message: 'Error interno del servidor' });
+    }
+}));
+router.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const vinyl = yield (0, getVinyls_1.getVinylById)(req.params);
+        return res.status(vinyl.status).json(vinyl.json);
+    }
+    catch (error) {
+        return res.status(404).json(error);
     }
 }));
 //! Ruta para iniciar sesión
@@ -41,17 +49,6 @@ router.get("/protectedResource", authMiddleware_1.authenticateJWT, (req, res) =>
     res.json({ message: 'Ruta protegida' });
 });
 router.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-
-router.get('/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const vinyl = yield (0, getVinyls_1.getVinylById)(req.params);
-        return res.status(vinyl.status).json(vinyl.json);
-    }
-    catch (error) {
-        return res.status(404).json(error);
-    }
-}));
-
     try {
         const vinyl = yield (0, postVinyl_1.postVinyl)(req.body);
         return res.status(vinyl.status).json(vinyl.json);
