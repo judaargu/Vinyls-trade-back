742bb7b0ac5e6ade75228f340a760697c60a4c5f
GMZ-Santi <santugomeztrgmail.com>
2023-09-07 11:46:30 -0300

    Seguimos avanzando

--- Api/src/Routes/index.ts
+++ Api/src/Routes/index.ts
@@ -1,26 +1,47 @@
 import { Router } from "express";
-import {getAllVinyls, postVinylsController} from "../Controllers/Vinyls/getVinyls";
-
-import { createUserHandler } from "../Routes/usersRouter";
-
+import { getAllVinyls, postVinylsController } from "../Controllers/Vinyls/getVinyls";
+import { createUser, loginUser } from "../Controllers/Users/postUser"; 
+import { authenticateJWT } from "../Middlewares/authMiddleware";
 import { postVinyl } from "../Controllers/Vinyls/postVinyl";
 import { Request, Response } from "express";
 
 const router = Router();
 
-router.get('/', getAllVinyls);
-router.post("/createUser", createUserHandler);
+router.get("/", getAllVinyls);
 
-router.post('/', async (req: Request, res: Response) => {
-    try {
-        const vinyl = await postVinyl(req.body);
-        return res.status(vinyl.status).json(vinyl.json);
+//! Ruta para registrar un nuevo usuario
+router.post("/createUser", async (req: Request, res: Response) => {
+  try {
+    const user = await createUser(req.body);
+    return res.status(user.status).json(user.data);
+  } catch (error) {
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
 
-    } catch (error) {
-        return res.status(404).json(error);
-        
-    }
+//! Ruta para iniciar sesión
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    const user = await loginUser(req.body);
+    return res.status(user.status).json(user.data);
+  } catch (error) {
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+router.get("/protectedResource", authenticateJWT, (req: Request, res: Response) => {
+  res.json({message: 'Ruta protegida'})
 })
 
-router.post("/vinyls", postVinylsController)
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const vinyl = await postVinyl(req.body);
+    return res.status(vinyl.status).json(vinyl.json);
+  } catch (error) {
+    return res.status(404).json(error);
+  }
+});
+
+router.post("/vinyls", postVinylsController);
+
 export default router;
