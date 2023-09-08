ffb1f0462ece8f7ea726e1056028c3f79b3b449c
Moonwalker231102 <ejimenezcadena@gmail.com>
2023-09-08 15:00:02 -0500

    Configuración para deploy

--- Api/index.ts
+++ Api/index.ts
@@ -1,7 +1,7 @@
 import server from './src/app'
 import {sequelize} from "./src/db";
-
-server.listen(3001, () => {
+import {PORT} from "./config";
+server.listen(PORT, () => {
   sequelize.sync({force: false})
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`Server listening on port: ${PORT}`); // eslint-disable-line no-console
   });
--- Api/src/app.js
+++ Api/src/app.js
@@ -18,7 +18,7 @@ server.use(body_parser_1.default.json({ limit: '50mb' }));
 server.use((0, cookie_parser_1.default)());
 server.use((0, morgan_1.default)('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
