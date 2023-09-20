6282a62226b81cb03a2cc6bbbb756b4f16feb593
Moonwalker231102 <ejimenezcadena@gmail.com>
2023-09-20 08:20:54 -0500

    Security commit

--- Api/src/Controllers/Vinyls/getVinyls.js
+++ Api/src/Controllers/Vinyls/getVinyls.js
@@ -8,20 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getVinylsController = exports.getVinylById = exports.postVinylsController = exports.getAllVinyls = void 0;
-const axios_1 = __importDefault(require("axios"));
 const Vinyls_1 = require("../../Models/Vinyls");
+const Users_1 = require("../../Models/Users");
 const postVinylsController = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = "5a6ec8f4-1e04-49c1-862a-bb35f1197e10";
     try {
-        const { data } = yield (0, axios_1.default)("http://localhost:3001/data.json");
-        const postVinyls = yield Promise.all(data.data.map((vinyl) => __awaiter(void 0, void 0, void 0, function* () {
-            const createdVinyl = yield Vinyls_1.Vinyl.create(vinyl);
-        })));
-        return res.status(200).json({ message: "Post successfully" });
+        // Busca al usuario por su userId
+        const user = yield Users_1.Users.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        const allVinyls = yield Vinyls_1.Vinyl.findAll();
+        // Asocia todos los vinilos al usuario
+        // await user.addVinyls(allVinyls);
+        res.status(200).json({ message: 'All vinyls associated with the user.' });
     }
     catch (error) {
         res.status(500).json({ error: error instanceof Error ? error.message : 'Something went wrong' });
@@ -29,8 +31,30 @@ const postVinylsController = (req, res) => __awaiter(void 0, void 0, void 0, fun
 });
 exports.postVinylsController = postVinylsController;
 const getVinylsController = () => __awaiter(void 0, void 0, void 0, function* () {
-    const findVinyl = yield Vinyls_1.Vinyl.findAll();
-    return findVinyl;
+    const findVinyls = yield Vinyls_1.Vinyl.findAll({
+        include: [
+            {
+                model: Users_1.Users,
+                as: 'Users',
+                through: {
+                    attributes: ['createdAt', 'UserId', 'VinylId']
+                }
+            }
+        ]
+    });
+    // Mapear y limpiar la respuesta
+    const cleanedVinyls = findVinyls.map((vinyl) => {
+        const { Users } = vinyl;
+        if (Users && Users.length > 0) {
+            return Object.assign(Object.assign({}, vinyl.toJSON()), { Users: Users.map((user) => ({
+                    UserVinyls: user.UserVinyls
+                })) });
+        }
+        else {
+            return vinyl.toJSON();
+        }
+    });
+    return cleanedVinyls;
 });
 exports.getVinylsController = getVinylsController;
 const getAllVinyls = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
--- Api/src/Controllers/Vinyls/getVinyls.ts
+++ Api/src/Controllers/Vinyls/getVinyls.ts
@@ -1,23 +1,59 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 import { Vinyl } from '../../Models/Vinyls';
-
-
+import { Users } from '../../Models/Users';
+import {Model} from "sequelize"
 const postVinylsController = async (req: Request, res: Response) => {
+    const userId: string = "5a6ec8f4-1e04-49c1-862a-bb35f1197e10"
     try {
-        const {data} = await axios("http://localhost:3001/data.json")
-        const postVinyls = await Promise.all(data.data.map(async (vinyl: {}) => {
-            const createdVinyl = await Vinyl.create(vinyl)
-        }))
-        return res.status(200).json({message: "Post successfully"})
+        // Busca al usuario por su userId
+        const user = await Users.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const allVinyls = await Vinyl.findAll(); 
+
+        // Asocia todos los vinilos al usuario
+        // await user.addVinyls(allVinyls);
+        res.status(200).json({ message: 'All vinyls associated with the user.' });
     } catch (error) {
-        res.status(500).json({ error: error instanceof Error ? error.message : 'Something went wrong' })
+        res.status(500).json({ error: error instanceof Error ? error.message : 'Something went wrong' });
     }
 }
-const getVinylsController = async () => {
-    const findVinyl = await Vinyl.findAll()
-    return findVinyl;
+const getVinylsController = async (): Promise<any[]> => {
+    const findVinyls = await Vinyl.findAll({
+        include: [
+            {
+                model: Users,
+                as: 'Users',
+                through: {
+                    attributes: ['createdAt', 'UserId', 'VinylId']
+                }
+            }
+        ]
+    });
+
+    // Mapear y limpiar la respuesta
+    const cleanedVinyls = findVinyls.map((vinyl: Model) => {
+        const { Users } = vinyl as any;
+        if (Users && Users.length > 0) {
+            return {
+                ...(vinyl.toJSON() as any),
+                Users: Users.map((user: Model) => ({
+                    UserVinyls: user.UserVinyls
+                }))
+            };
+        } else {
+            return vinyl.toJSON() as any;
+        }
+    });
+
+    return cleanedVinyls;
 }
+
+
 const getAllVinyls = async (req: Request, res: Response) => {
     try {
         const response = await getVinylsController();
